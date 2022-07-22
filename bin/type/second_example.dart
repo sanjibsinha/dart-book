856cd4d3033afa4fa0c1f0e58eda3596cc851d30
Sanjib Sinha <sanjib12sinha@gmail.com>
2022-07-22 12:51:41 +0530

    Type second

--- bin/type/second_example.dart
+++ bin/type/second_example.dart
@@ -8,6 +8,9 @@ void main(List<String> args) {
   print(nameVariable);
   print(nameObject);
   print(nameString);
+  nameVariable = 100 as String;
+  nameObject = 100;
+  nameString = 100 as String;
 }
 
 /**
