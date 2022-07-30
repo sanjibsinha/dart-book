7d1a1c6551ce644090abc5a7b1e7aa26440aaad0
Sanjib Sinha <sanjib12sinha@gmail.com>
2022-07-30 17:07:48 +0530

    Third

---
+++ bin/inference/second.dart
@@ -0,0 +1,11 @@
+void main(List<String> args) {
+  var x = 1;
+  //x = 1.1; // A value of double can't be assigned to variable of type int
+
+  x = 2; // it's okay
+  print(x); // 2
+
+  num y = 1;
+  y = 45.56; // it's okay
+  print(y); // 45.56
+}
